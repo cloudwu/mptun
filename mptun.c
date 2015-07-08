08bf9df434e1276cedfa3e525092bc46fb19fc84
Cloud Wu <cloudwu@gmail.com>
2015-07-08 20:40:36 +0800

    add rc4 encrypt

--- mptun.c
+++ mptun.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 #include <sys/socket.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
@@ -16,6 +17,7 @@
 #include <stdarg.h>
 #include <signal.h>
 #include <inttypes.h>
+#include <time.h>
 
 #define MAX_ADDRESS 16
 #define BASE_COUNT 64
@@ -23,6 +25,8 @@
 /* buffer for reading , must be >= 1500 */
 #define BUFF_SIZE 2000   
 #define IP_SIZE 128
+/* 1 hour time diff */
+#define TIME_DIFF 3600
 
 // todo: support ipv6
 typedef struct sockaddr_in SOCKADDR;
@@ -31,6 +35,8 @@ typedef struct in_addr INADDR;
 static int SIG = 0;
 
 struct tundev {
+	uint64_t key;
+	time_t ti;
 	int port;
 	int tunfd;
 	int remote_n;
@@ -44,12 +50,180 @@ struct tundev {
 	uint64_t out[MAX_ADDRESS];
 	uint64_t drop;
 	uint64_t untrack;
+	uint64_t invalid;
 };
+
+struct rc4_sbox {
+	int i;
+	int j;
+	uint8_t sbox[256];
+};
+
+static void
+rc4_init(struct rc4_sbox *rs, uint64_t seed) {
+	rs->i=0;
+	rs->j=0;
+	int i;
+	uint8_t k[8];
+	for (i=0;i<8;i++) {
+		k[i] = seed & 0xff;
+		seed >>= 8;
+	}
+	for (i=0;i<256;i++) {
+		rs->sbox[i] = (uint8_t)((i + k[i%8]) & 0xff);
+	}
+}
+
+static void
+rc4_encode(struct rc4_sbox *rs, const uint8_t *src, uint8_t *des, size_t sz) {
+	size_t i;
+	for (i=0;i<sz;i++) {
+		rs->i = (rs->i + 1) % 256;
+		rs->j = (rs->j + rs->sbox[rs->i]) % 256;
+		uint8_t si = rs->sbox[rs->i];
+		uint8_t sj = rs->sbox[rs->j];
+		rs->sbox[rs->i] = sj;
+		rs->sbox[rs->j] = si;
+		uint8_t d = src[i] ^ rs->sbox[(si+sj) % 256];
+		des[i] = d;
+	}
+}
+
+static uint64_t
+hash_key(const char * str, int sz) {
+	uint32_t djb_hash = 5381L;
+	uint32_t js_hash = 1315423911L;
+
+	int i;
+	for (i=0;i<sz;i++) {
+		uint8_t c = (uint8_t)str[i];
+		djb_hash += (djb_hash << 5) + c;
+		js_hash ^= ((js_hash << 5) + c + (js_hash >> 2));
+	}
+
+	return (uint64_t) djb_hash << 32 | js_hash;
+}
+
+// leftrotate function definition
+#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+
+static uint64_t
+hmac(uint64_t x, uint64_t y) {
+// Constants are the integer part of the sines of integers (in radians) * 2^32.
+	static const uint32_t k[64] = {
+		0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee ,
+		0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501 ,
+		0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be ,
+		0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821 ,
+		0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa ,
+		0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8 ,
+		0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed ,
+		0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a ,
+		0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c ,
+		0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70 ,
+		0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05 ,
+		0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665 ,
+		0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039 ,
+		0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1 ,
+		0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1 ,
+		0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 };
+ 
+// r specifies the per-round shift amounts
+static const uint32_t r[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+					  5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+					  4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+					  6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+ 
+	uint32_t w[16];
+	uint32_t a, b, c, d, f, g, temp;
+	int i;
+ 
+	a = 0x67452301u;
+	b = 0xefcdab89u;
+	c = 0x98badcfeu;
+	d = 0x10325476u;
+
+	for (i=0;i<16;i+=4) {
+		w[i] = (uint32_t)(x << 32);
+		w[i+1] = (uint32_t)x;
+		w[i+2] = (uint32_t)(y << 32);
+		w[i+3] = (uint32_t)y;
+	}
+
+	for(i = 0; i<64; i++) {
+		if (i < 16) {
+			f = (b & c) | ((~b) & d);
+			g = i;
+		} else if (i < 32) {
+			f = (d & b) | ((~d) & c);
+			g = (5*i + 1) % 16;
+		} else if (i < 48) {
+			f = b ^ c ^ d;
+			g = (3*i + 5) % 16; 
+		} else {
+			f = c ^ (b | (~d));
+			g = (7*i) % 16;
+		}
+
+		temp = d;
+		d = c;
+		c = b;
+		b = b + LEFTROTATE((a + f + k[i] + w[g]), r[i]);
+		a = temp;
+
+	}
+
+	return (uint64_t)(a^b) << 32 | (c^d);
+}
+
+static int
+encrypt(const char in[BUFF_SIZE], int sz, char out[BUFF_SIZE], uint64_t key, time_t ti) {
+	uint64_t h = hash_key(in, sz);
+	uint32_t tmp;
+	struct rc4_sbox rs;
+	if (sz > BUFF_SIZE - 8)
+		return -1;
+	key = hmac(key, ti);
+	rc4_init(&rs, key);
+	key ^= h;
+	tmp = htonl(ti);
+	memcpy(out, &tmp, 4);
+	tmp = htonl((uint32_t)key ^ (uint32_t)(key >> 32));
+	memcpy(out+4, &tmp, 4);
+	rc4_encode(&rs, (const uint8_t *)in, (uint8_t *)out+8, sz);
+
+	return sz + 8;
+}
+
+static int
+decrypt(const char in[BUFF_SIZE], int sz, char out[BUFF_SIZE], uint64_t key, time_t ti) {
+	uint32_t pt, check, h;
+	struct rc4_sbox rs;
+	sz -= 8;
+	if (sz < 0)
+		return -1;
+
+	memcpy(&pt, in, 4);
+	memcpy(&check, in+4, 4);
+	pt = ntohl(pt);
+	check = ntohl(check);
+	if (abs((int)(pt - ti)) > TIME_DIFF)
+		return -1;
+	key = hmac(key, pt);
+	rc4_init(&rs, key);
+
+	rc4_encode(&rs, (const uint8_t *)in+8, (uint8_t *)out, sz);
+	h = hash_key(out, sz);
+	key ^= h;
+
+	if (check != key)
+		return -1;
+	return sz;
+}
 
 static void
 dumpinfo(struct tundev *tdev) {
 	char tmp[1024];
-//	inet_ntop(AF_INET, &sa->sin_addr, tmp, sizeof(tmp));
 	int i;
 	uint64_t s = 0;
 	for (i=0;i<tdev->local_n;i++) {
@@ -182,7 +356,7 @@ inet_to_tun(struct tundev *tdev, int index) {
 	SOCKADDR sa;
 	int inetfd = tdev->localfd[index];
 	int tunfd = tdev->tunfd; 
-	char buf[BUFF_SIZE];
+	char buf[BUFF_SIZE], outbuff[BUFF_SIZE];
 	ssize_t n;
 	for (;;) {
 		socklen_t addrlen = sizeof(sa);
@@ -201,11 +375,15 @@ inet_to_tun(struct tundev *tdev, int index) {
 		}
 	}
 
-//	char tmp[1024];
-//	inet_ntop(AF_INET, &sa->sin_addr, tmp, sizeof(tmp));
-//	printf("Read %d bytes from inet %d %s:%d\n", (int)n, inetfd, tmp, ntohs(sa->sin_port));
+	n = decrypt(buf, n, outbuff, tdev->key, tdev->ti);
+
+	if (n < 0) {
+		tdev->invalid += n;
+		return;
+	}
+
 	for (;;) {
-		int ret = write(tunfd, buf, n);
+		int ret = write(tunfd, outbuff, n);
 		if (ret < 0) {
 			if (errno == EINTR) {
 				continue;
@@ -218,7 +396,6 @@ inet_to_tun(struct tundev *tdev, int index) {
 			break;
 		}
 	}
-//	printf("Write %d bytes to tun %d\n", (int)n, tunfd);
 
 	// succ
 	add_remote(tdev, &sa, (int)n);
@@ -304,7 +481,7 @@ tun_to_inet(struct tundev *tdev, fd_set *wt) {
 	int inetfd = tdev->localfd[localindex];
 	int remoteindex = choose_remote(tdev);
 	SOCKADDR * addr = &tdev->remote[remoteindex];
-	char buf[BUFF_SIZE];
+	char buf[BUFF_SIZE], outbuf[BUFF_SIZE];
 	ssize_t n;
 	for (;;) {
 		n = read(tunfd, buf, BUFF_SIZE);
@@ -321,10 +498,15 @@ tun_to_inet(struct tundev *tdev, fd_set *wt) {
 			break;
 		}
 	}
-//	printf("Read %d bytes from tun %d\n", (int)n, tunfd);
+
+	n = encrypt(buf, n, outbuf, tdev->key, tdev->ti);
+	if (n < 0) {
+		fprintf(stderr, "Invalid tun package size %d", (int)n);
+		return;
+	}
 
 	for (;;) {
-		int ret = sendto(inetfd, buf, n, 0, (struct sockaddr *)addr, sizeof(SOCKADDR));
+		int ret = sendto(inetfd, outbuf, n, 0, (struct sockaddr *)addr, sizeof(SOCKADDR));
 		if (ret < 0 && errno == EINTR) {
 			continue;
 		} else {
@@ -332,11 +514,6 @@ tun_to_inet(struct tundev *tdev, fd_set *wt) {
 		}
 	}
 	tdev->out[remoteindex] += n;
-
-//	char tmp[1024];
-//	inet_ntop(AF_INET, &addr->sin_addr, tmp, sizeof(tmp));
-
-//	printf("Write %d bytes to inet %d %s:%d\n", (int)n, inetfd, tmp, ntohs(addr->sin_port));
 }
 
 static void
@@ -433,6 +610,7 @@ start(struct tundev *tdev) {
 
 	for (;;) {
 		dumpinfo_hup(tdev);
+		tdev->ti = time(NULL);
 		forwarding(tdev, maxrd_fd+1, &rdset, maxwt_fd+1, &wtset);
 	}
 }
@@ -458,7 +636,7 @@ main(int argc, char *argv[]) {
 	struct tundev tdev;
 	memset(&tdev, 0, sizeof(tdev));
 
-	while ((option = getopt(argc, argv, "i:v:t:r:l:p:")) > 0) {
+	while ((option = getopt(argc, argv, "i:v:t:r:l:p:k:")) > 0) {
 		INADDR addr;
 		switch(option) {
 		case 'i':
@@ -494,6 +672,9 @@ main(int argc, char *argv[]) {
 				++tdev.remote_n;
 				sa->sin_addr = addr;
 			}
+			break;
+		case 'k':
+			tdev.key = hash_key(optarg, strlen(optarg));
 			break;
 		default:
 			usage();
